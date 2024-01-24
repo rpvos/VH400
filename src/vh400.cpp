5ee469535c1d8d4139d5a9616ad3d2f1c19625c0
Rik <rik.vos01@gmail.com>
2024-01-24 15:13:27 +0100

    Fix below 0% and above 100%

--- src/vh400.cpp
+++ src/vh400.cpp
@@ -3,29 +3,38 @@
 float Vh400::CalculateVWC(float voltage)
 {
     // 0 to 1.1V	    VWC = 10*V-1
-    if (0 <= voltage && voltage < 1.1)
+    if (voltage < 1.1f)
     {
-        return 10 * voltage - 1;
+        if (voltage >= 0.1f)
+        {
+            return 10 * voltage - 1;
+        }
+        return 0.0f;
     }
     // 1.1V to 1.3V	    VWC = 25*V- 17.5
-    else if (1.1 <= voltage && voltage < 1.3)
+    else if (1.1f <= voltage && voltage < 1.3f)
     {
-        return 25 * voltage - 17.5;
+        return 25 * voltage - 17.5f;
     }
     // 1.3V to 1.82V	VWC = 48.08*V- 47.5
-    else if (1.3 <= voltage && voltage < 1.82)
+    else if (1.3f <= voltage && voltage < 1.82f)
     {
-        return 48.08 * voltage - 47.5;
+        return 48.08f * voltage - 47.5f;
     }
     // 1.82V to 2.2V	VWC = 26.32*V- 7.89
-    else if (1.82 <= voltage && voltage < 2.2)
+    else if (1.82f <= voltage && voltage < 2.2f)
     {
-        return 26.32 * voltage - 7.89;
+        return 26.32f * voltage - 7.89f;
     }
     // 2.2V - 3.0V	    VWC = 62.5*V - 87.5
-    else if (2.2 <= voltage && voltage <= 3)
+    else if (2.2f <= voltage && voltage <= 3.0f)
     {
-        return 62.5 * voltage - 87.5;
+        if (voltage > 3.0f)
+        {
+            return 100.0f;
+        }
+
+        return 62.5f * voltage - 87.5f;
     }
 
     // If no correct voltage return -1
